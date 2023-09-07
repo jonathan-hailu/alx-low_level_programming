b30ea0cd9411bda47b43c88b996d4d5fa7879edd
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-09-07 10:06:53 -0700

    edit on 6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,8 @@
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an double: %lu byte(s)\n", sizeof(double));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
