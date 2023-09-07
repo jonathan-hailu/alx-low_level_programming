393ae4e0765c1de3810225234ff65a3e74578528
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-09-07 08:47:35 -0700

    edited the 6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
 int main(void)
 {
+	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an double: %lu byte(s)\n", sizeof(double));
