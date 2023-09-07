348634ce72fd53a7cc48594ad99f3ff0a264c501
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-09-07 09:55:34 -0700

    edited the 6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*This program first includes the stdio.h header file, which contains the printf() function. Then, it defines a main() function, which prints the size of each of the following types: */
-int main() {
-  printf("Size of char: %zu\n", sizeof(char));
-  printf("Size of int: %zu\n", sizeof(int));
-  printf("Size of long int: %zu\n", sizeof(long int));
-  printf("Size of float: %zu\n", sizeof(float));
-  printf("Size of double: %zu\n", sizeof(double));
-  printf("Size of long double: %zu\n", sizeof(long double));
-
-  return 0;
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+return (0);
 }
