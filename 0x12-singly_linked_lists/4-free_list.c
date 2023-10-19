20a6c1061c8d3961b2f7633a31ae02cbe2df6c51
Yonatan Hailu <jonathanhailu3@gmail.com>
2023-10-19 01:05:23 -0700

    Finish

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+
+/**
+ *  * free_list - Free all the list with return NULL
+ *   * @head: the head pointer
+ *    * Return: retunrn the printed value
+ *     */
+
+void free_list(list_t *head)
+{
+		if (head != NULL)
+				{
+							if (head->str != NULL)
+											free(head->str);
+									free_list(head->next);
+											free(head);
+												}
+
+}
